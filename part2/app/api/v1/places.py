37e1e0e688dccaf8c2b31cb58de32029758ceab0
Andrew Perry <pezza76@hotmail.com>
2025-06-19 13:24:56 +1000

    test

--- part2/app/api/v1/places.py
+++ part2/app/api/v1/places.py
@@ -1,5 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from app.models.place import Place
 
 api = Namespace('places', description='Place operations')
 
@@ -17,7 +18,7 @@ user_model = api.model('PlaceUser', {
 })
 
 # Define the place model for input validation and documentation
-place_model = api.model('Place', {
+place_input_model = api.model('Place', {
     'title': fields.String(required=True, description='Title of the place'),
     'description': fields.String(description='Description of the place'),
     'price': fields.Float(required=True, description='Price per night'),
@@ -27,21 +28,35 @@ place_model = api.model('Place', {
     'amenities': fields.List(fields.String, required=True, description="List of amenities ID's")
 })
 
+place_output_model = api.inherit('PlaceOutput', place_input_model, {
+    'id': fields.String(description='ID of the place')
+})
+
 @api.route('/')
 class PlaceList(Resource):
-    @api.expect(place_model)
+    @api.expect(place_input_model)
     @api.response(201, 'Place successfully created')
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new place"""
         # Placeholder for the logic to register a new place
-        pass
+        data = api.payload
 
+        if not data.get('title') or not data.get('price') or not data.get('latitude') or not data.get('longitude') or not data.get('owner_id'):
+            return {'message': 'Missing required fields'}, 400
+
+
+        new_place = Place(**data)
+        facade.place_repo.add(new_place)
+
+        return {'message': 'Place successfully created'}, 201
+    
+    @api.marshal_list_with(place_output_model)
     @api.response(200, 'List of places retrieved successfully')
     def get(self):
         """Retrieve a list of all places"""
         # Placeholder for logic to return a list of all places
-        pass
+        return facade.place_repo.get_all()
 
 @api.route('/<place_id>')
 class PlaceResource(Resource):
@@ -50,9 +65,9 @@ class PlaceResource(Resource):
     def get(self, place_id):
         """Get place details by ID"""
         # Placeholder for the logic to retrieve a place by ID, including associated owner and amenities
-        pass
+        
 
-    @api.expect(place_model)
+    @api.expect(place_input_model)
     @api.response(200, 'Place updated successfully')
     @api.response(404, 'Place not found')
     @api.response(400, 'Invalid input data')
@@ -60,3 +75,6 @@ class PlaceResource(Resource):
         """Update a place's information"""
         # Placeholder for the logic to update a place by ID
         pass
+
+
+    this is a test
