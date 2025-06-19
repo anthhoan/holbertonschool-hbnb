a8171928f4da6c5c12e058a31b69633743638e1c
anthhoang <anthhoan@gmail.com>
2025-06-19 14:48:26 +1000

    amenity error handling

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -7,13 +7,19 @@ class Amenity:
         self.name = name
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        
+
+    # Amenity name
     @property
     def name(self):
         return self._name
-        
+
     @name.setter
     def name(self, name):
+        value = value.strip()
+        if len(value) <= 50:
+            self._name = value
+        else:
+            raise ValueError("Amenity name cannot exceed 50 characters")
         if not isinstance(name, str):
             raise TypeError("name must be a string")
         self._name = name
