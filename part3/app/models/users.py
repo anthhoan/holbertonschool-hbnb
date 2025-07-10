7252754efe8e877c67fad0e57731af81b13602e8
Jin Liu <iamjincn@outlook.com>
2025-07-10 19:44:46 +1000

    task 8: user models

--- part3/app/models/users.py
+++ part3/app/models/users.py
@@ -2,17 +2,20 @@ import uuid
 from app import db, bcrypt
 from datetime import datetime
 import re
-from baseclass import BaseModel
+from app.models.baseclass import BaseModel
+from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     __tablename__ = 'users'
 
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    password = db.Column(db.String(128), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
+    _first_name = db.Column(db.String(50), nullable=False)
+    _last_name = db.Column(db.String(50), nullable=False)
+    _email = db.Column(db.String(120), nullable=False, unique=True)
+    _password = db.Column(db.String(128), nullable=False)
+    _is_admin = db.Column(db.Boolean, default=False)
 
+    places_r = relationship("Place", back_populates="owner_r")
+    reviews_r = relationship("Review", back_populates="user_r")
 
     def hash_password(self, password):
         """Hash the password before storing it."""
@@ -21,13 +24,14 @@ class User(BaseModel):
     def verify_password(self, password):
         """Verify the hashed password."""
         return bcrypt.check_password_hash(self.password, password)
-    
-    def __init__(self, first_name, last_name, email, is_admin=False):
+
+    def __init__(self, first_name, last_name, email, password=None, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+        self.hash_password(password)
         self.places = [] # User owned places
         self.reviews = [] # User owned reviews
 
@@ -43,7 +47,7 @@ class User(BaseModel):
     def first_name(self, value):
         """First name Setter, raises a ValueError if first name is more than 50 characters or less than 2 characters"""
         value = value.strip()
-        if len(value) <= 50 or len(value) < 2:
+        if len(value) <= 50 and len(value) >= 2:
             self._first_name = value
         else:
             raise ValueError("First name must be between 2-50 characters.")
@@ -60,7 +64,7 @@ class User(BaseModel):
     def last_name(self, value):
         """Last name Setter, raises a ValueError if last name is more than 50 characters or less than 2 characters"""
         value = value.strip()
-        if len(value) <= 50 or len(value) < 2:
+        if len(value) <= 50 and len(value) >= 2:
             self._last_name = value
         else:
             raise ValueError("Last name must be between 2-50 characters")
